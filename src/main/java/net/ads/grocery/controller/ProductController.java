6cb98a3d0ed9b989e5601ec7fb29efba55d36f2e
Ashi-Ashirbaditya <ashirbaditya@gmail.com>
2025-11-13 15:14:12 +0530

    css

--- src/main/java/net/ads/grocery/controller/ProductController.java
+++ src/main/java/net/ads/grocery/controller/ProductController.java
@@ -8,6 +8,7 @@ import net.ads.grocery.service.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -83,7 +84,7 @@ public class ProductController {
     }
 
     // Delete an product
-    @GetMapping("/deleteProducts/{id}")
+   @GetMapping("/deleteProducts/{id}")
     public String deleteProducts(@PathVariable("id") long id) {
         this.productService.deleteProductsById(id);
         return "redirect:/product";
